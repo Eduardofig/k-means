8b57b8ec80e42d22649fc6fbd9145b8594419b6c
Eduardofig <duzinho039@usp.br>
2021-02-12 18:03:09 +0000

    limit for centroid coordinates

--- k-means.cpp
+++ k-means.cpp
@@ -14,15 +14,23 @@ typedef struct {
 
 std::vector<cluster> k_means(std::vector<point> points, int num_centroids)
 {
+    int max_x = 0, max_y = 0, c_index, c_min_dist, c_compare_dist;
+    for (point p : points) {
+        if (p.x > max_x) {
+        max_x = p.x;
+        }
+        if(p.y > max_y) {
+            max_y = p.y;
+        }
+    }
     std::vector<cluster> vec_cl;
     std::vector<point> centroids;
     cluster c;
     for (int i = 0; i < num_centroids; ++i) {
-       c.centroid.x = rand();
-       c.centroid.y = rand();
+       c.centroid.x = rand() % max_x;
+       c.centroid.y = rand() % max_y;
        vec_cl.push_back(c);
     };
-    int c_index, c_min_dist, c_compare_dist;
     for(point p : points) {
         c_index = 0, c_min_dist = (sqrt((p.x - vec_cl[0].centroid.x)^2 + (p.y - vec_cl[0].centroid.y)^2));
         for(int i = 0; i < vec_cl.size(); ++i) {
